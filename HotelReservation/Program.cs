fcb8ce446ddd741e24f4277bb16ea7fe78ab1ff6
Emmanuel <o.emmanuelozioma@gmail.com>
2022-08-28 20:04:11 +0100

    Initial Commit

---
+++ HotelReservation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelReservation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
